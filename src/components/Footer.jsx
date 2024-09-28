e665cf67f80338464fccecfc07946c6ea94bc8fe
korinagoldsmith <korinagoldsmith@gmail.com>
2024-09-27 22:16:39 -0400

    readme updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,8 +20,6 @@ const Footer = (props) => {
         using <i className="fab fa-react" />
         <p>
           <small className="text-muted">
-            Project code is open source. Feel free to fork and make your own
-            version.
           </small>
         </p>
       </Container>
